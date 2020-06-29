f6aab9562c32484e6ebddce41864d5deccfc0f3e
sandi sunandar <sandisunandar99@gmail.com>
2020-06-29 09:39:18 +0700

    reformated index models for support hapi 17

--- models/index.js
+++ models/index.js
@@ -2,25 +2,23 @@ const mongoose = require('mongoose');
 const config = require('../config/config');
 const bluebird = require('bluebird');
 
-const register = (server, options, next) => {
-  mongoose.Promise = bluebird;
-  mongoose.connect(config.database.uri, config.database.options, (err, db) => {
-    if (err) console.log(err);
 
-    require('./User')
+exports.plugin = {
+  name: "models", 
+  register: async (server, options) => {
+    mongoose.Promise = bluebird;
+    mongoose.connect(config.database.uri, config.database.options, (err, db) => {
+      if (err) console.log(err);
+
+      require('./User')
 
 
-    server.app.db = {
-      link: db.db,
-      User: db.model('User')
-    };
+      server.app.db = {
+        link: db.db,
+        User: db.model('User')
+      };
 
-    return next();
-  });
-};
-
-register.attributes = {
-  pkg: require('./package.json')
+      return "ok"
+    });
+  }
 }
-
-module.exports = register;
